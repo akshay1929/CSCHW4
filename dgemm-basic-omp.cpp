0edcfb8bcc1bf476b4dbd94788b9f9cec5263178
Akshay Mhatre <35747718+akshay1929@users.noreply.github.com>
2022-10-09 16:53:11 -0700

    Add files via upload

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -17,11 +17,11 @@ void square_dgemm(int n, double* A, double* B, double* C)
    // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
    // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
    // after the matrix multiply code but before the end of the parallel code block.
-   //#pragma omp parallel 
-   //{
+   #pragma omp parallel 
+   {
       LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
-      #pragma omp parallel for
       
+      #pragma omp for
       for (int i = 0; i < n; i++) {
          for (int j = 0; j < n; j++) {
             for (int k = 0; k < n; k++) {
@@ -30,5 +30,5 @@ void square_dgemm(int n, double* A, double* B, double* C)
          }
       }
       LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
-   //}
+   }
 }
